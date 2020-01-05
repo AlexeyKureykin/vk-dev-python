20af5519a240adef1c04d221c86de6e056bdde77
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2020-01-05 12:44:18 +0300

    Скрипт для поиска самого популярного друга

---
+++ vk.freinds-max.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import time
+
+f = open('token.txt', 'r')
+token = f.read()
+f.close()
+
+#Получить список id своих друзей
+params = {'v':'5.52', 'access_token':token}
+r = requests.get('https://api.vk.com/method/friends.get?', params=params)
+friends = json.loads(r.content)
+
+max_count = 0
+max_user_id = 0
+
+for friend in friends['response']['items']:
+    params = {'user_id' : friend, 'v' : '5.52', 'access_token':token}
+    r = requests.get('https://api.vk.com/method/friends.get?', params=params)
+    f = json.loads(r.content)
+    count = f['response']['count']  #Получить число друзей у друга
+    if count > max_count:
+        max_count = count
+        max_user_id = friend
+    
+    time.sleep(0.1)
+print(max_user_id, max_count)
