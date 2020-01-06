b199e95a318c5f49d2786e87eb6f9c23b29fa8bb
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2020-01-06 12:16:13 +0300

    Вывод имени и фамилии популярного друга

--- vk.freinds-max.py
+++ vk.freinds-max.py
@@ -2,12 +2,20 @@ import requests
 import json
 import time
 
-f = open('e:\token.txt', 'r')
+f = open('token.txt', 'r')
 token = f.read()
 f.close()
 
+def get_name_by_id(id):
+    params = {'user_ids':max_user_id, 'v':'5.52', 'access_token':token}
+    r = requests.get('https://api.vk.com/method/users.get?', params=params)
+    j = json.loads(r.content)
+    first_name = j['response'][0]['first_name']
+    last_name = j['response'][0]['last_name']
+    return first_name + ' ' + last_name
+
 #Получить список id своих друзей
-params = {'v':'5.52', 'access_token':token}
+params = {'v':'5.52', 'access_token':token, 'count':10, 'order':'hints'}
 r = requests.get('https://api.vk.com/method/friends.get?', params=params)
 friends = json.loads(r.content)
 
@@ -18,10 +26,11 @@ for friend in friends['response']['items']:
     params = {'user_id' : friend, 'v' : '5.52', 'access_token':token}
     r = requests.get('https://api.vk.com/method/friends.get?', params=params)
     f = json.loads(r.content)
+    
     count = f['response']['count']  #Получить число друзей у друга
     if count > max_count:
         max_count = count
         max_user_id = friend
     
-    time.sleep(0.1)
-print(max_user_id, max_count)
+    time.sleep(1)
+print(max_user_id, get_name_by_id(max_user_id), max_count)
